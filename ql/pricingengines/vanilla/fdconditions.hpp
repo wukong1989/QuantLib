d7ea21126287395ecbe1e7d4db36ac548ef701e8
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-01-29 12:04:20 +0100

    Deprecate an unused part of the old FD framework.

--- ql/pricingengines/vanilla/fdconditions.hpp
+++ ql/pricingengines/vanilla/fdconditions.hpp
@@ -32,8 +32,11 @@
 
 namespace QuantLib {
 
+    /*! \deprecated Use the new framework instead.
+                    Deprecated in version 1.22.
+    */
     template <typename baseEngine>
-    class FDAmericanCondition : public baseEngine {
+    class QL_DEPRECATED FDAmericanCondition : public baseEngine {
       public:
         FDAmericanCondition(
              const ext::shared_ptr<GeneralizedBlackScholesProcess>& process,
